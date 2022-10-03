7964ba844f748c941cda6a803225ef5f38d8ba3e
madalynmm <madalynmm@outlook.com>
2022-10-03 13:15:55 -0400

    added functions for password options and character arrays

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,64 @@
-// Assignment code here
+// Array of special characters:
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
+// Array of numeric characters:
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+// Array of lower case characters:
+var lowerCaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+// Array of upper case characters:
+var upperCaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 
-
+// Assignment code
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Function for getting random element from an array:
+function getRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[randomIndex];
+
+  return randomElement;
+}
+
+function getPasswordOptions() {
+  //variable to store length of password from user
+  var length = parseInt(
+    prompt("How many characters would you like your password to be?"),
+    10
+  );
+    if (Number.isNaN(length)) {
+      alert("Password length must be provided as a number.");
+      return null;
+    };
+    if (length <8 || length >128) {
+      alert("Password length must be between 8 and 128 characters.");
+      return null;
+    }
+
+  var hasSpecialCharacters = confirm("Click OK to confirm including special characters in your password.");
+
+
+  //Object to store user input:
+  var passwordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+
+  };
+
+  return passwordOptions;
+}
+
+
+function generatePassword() {
+   var options = getPasswordOptions();
+  //  code logic below?
+  return "password";
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var length = window.prompt("Enter password length:");
-    
-      if (!length) {
-        return;
-      }
-      
-      if (length < 8 || length > 128) {
-        window.alert("Please choose a number between 8 and 128.");
-        return;
-      }
-      // else ()
-
-  var caseValue =window.prompt("Do you want to include uppercase letters, lowercase letters, or both?")
-      
-      if (!caseValue) {
-        return;
-      }
-
-      if (caseValue = "uppercase") {
-        // add uppercase parameters to random generator
-      }
-      else if (caseValue = "lowercase") {
-        // add lowercse parameters to generator
-      }
-      else if (caseValue = "both") {
-        // add both
-      }
-
-  var numValue = window.prompt("Do you want to include numbers?")
-
-    if (!numValue) {
-      return;
-    }
-
-    if (numValue = "yes") {
-      // use numbers
-    }
-    else if (numValue = "no") {
-      // skip numbers
-    }
-
-  var specialChars = window.prompt("Do you want to include special characters?");
-
-    if (!specialChars) {
-      return;
-    }
-
-    if (specialChars = "yes") {
-      // add special characters
-    }
-    else if (specialChars = "no") {
-      // skip special characters
-    }
-
-    
-
+  
   passwordText.value = password;
 
 }
